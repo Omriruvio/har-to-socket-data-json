ba6140ebf925676d9470b28ee18d87ab1ed5b544
Omri Ruvio <omriruvio@gmail.com>
2023-03-01 00:08:05 +0200

    docs: add -h flag

--- index.ts
+++ index.ts
@@ -2,9 +2,6 @@ import { harToSocketDataJson } from './harToSocketDataJson';
 import { getArgs } from './utils/getArgs';
 import { getHarFileFromPrompt } from './utils/harFromPrompt';
 
-// Parse command line arguments
-// supports -f and --f for file path
-// supports -o and --o for output path
 const { harFilePath, outputFilePath } = getArgs();
 
 (async () => {
--- utils/getArgs.ts
+++ utils/getArgs.ts
@@ -1,4 +1,11 @@
 import * as fs from 'fs';
+import chalk from 'chalk';
+const log = console.log;
+
+// Parse command line arguments
+// supports -f and --f for file path
+// supports -o and --o for output path
+// support -h and --h for help
 
 export const getArgs = () => {
   let harFilePath: string | undefined;
@@ -15,14 +22,38 @@ export const getArgs = () => {
     harFilePath = argsMap.get('--f') || argsMap.get('-f');
     if (!harFilePath?.endsWith('.har')) harFilePath = `${harFilePath}.har`;
     if (!fs.existsSync(harFilePath)) {
-      console.log(`File ${harFilePath} does not exist, please try again`);
+      log(`File path is not valid, please try again`);
       process.exit(1);
     }
   }
 
   if (argsMap.has('--o') || argsMap.has('-o')) {
     outputFilePath = argsMap.get('--o') || argsMap.get('-o');
-    console.log(`Setting output path to ${outputFilePath}`);
+    if (!outputFilePath) {
+      log(`Output path is not valid, please try again`);
+      process.exit(1);
+    }
+    log(`Setting output path to ${outputFilePath}`);
+  }
+
+  if (argsMap.has('--h') || argsMap.has('-h')) {
+    log(chalk.blue(`                                                                   `));
+    log(chalk.blue(`██╗  ██╗ █████╗ ██████╗ ██████╗      ██╗███████╗ ██████╗ ███╗   ██╗`));
+    log(chalk.blue(`██║  ██║██╔══██╗██╔══██╗╚════██╗     ██║██╔════╝██╔═══██╗████╗  ██║`));
+    log(chalk.blue(`███████║███████║██████╔╝ █████╔╝     ██║███████╗██║   ██║██╔██╗ ██║`));
+    log(chalk.blue(`██╔══██║██╔══██║██╔══██╗██╔═══╝ ██   ██║╚════██║██║   ██║██║╚██╗██║`));
+    log(chalk.blue(`██║  ██║██║  ██║██║  ██║███████╗╚█████╔╝███████║╚██████╔╝██║ ╚████║`));
+    log(chalk.blue(`╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝`));
+    log(chalk.blue(`                                                                   `));
+    log(chalk.yellow(`                  Created by: Omri Ruvio                         `));
+    log(chalk.blue(`
+    This tool will convert a HAR file to a JSON file.
+    Output includes Socket events data & timing.
+    `));
+    log(chalk.blue("Usage: ts-node index.js -f=" + chalk.yellow("<path to har file>") + " -o=" + chalk.yellow("<path to output file>")));
+
+    // log(`Usage: node index.js --f=<path to har file> --o=<path to output file>`);
+    process.exit(0);
   }
 
   return { harFilePath, outputFilePath };
